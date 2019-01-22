5d11b96f2dc2757f50b5bea4f4ede0ad97903b57
Ya Wen <36779592+YasmineW@users.noreply.github.com>
2019-01-22 15:20:49 +1300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,404 @@
+//set different photo album 
+//add new wedding photo on top 
+const weddingAlbum = [
+  "images/thumbnail/wedding/Shi & Jie's wedding-14.jpg",
+  "images/thumbnail/wedding/Han Feng & Chen Hui's wedding - 032.jpg",
+  "images/thumbnail/wedding/Han Feng & Chen Hui's wedding - 052.jpg",  
+  "images/thumbnail/wedding/Chen & Li's wedding _ 071.jpg",
+  "images/thumbnail/wedding/Jan & Leo's Wedding_48.jpg",
+  "images/thumbnail/wedding/Liu & Ding's wedding _ 04.jpg",
+  "images/thumbnail/wedding/Han Feng & Chen Hui's wedding - 148.jpg",
+  "images/thumbnail/wedding/Huang & Chen's wedding _ 13.jpg",
+  "images/thumbnail/wedding/Hannah & Shane's wedding _ 02.jpg",
+  "images/thumbnail/wedding/Shi & Jie's wedding-33.jpg",
+  "images/thumbnail/wedding/Han Feng & Chen Hui's wedding - 137.jpg",
+  "images/thumbnail/wedding/Hannah & Shane's wedding _ 12.jpg",
+  "images/thumbnail/wedding/Han Feng & Chen Hui's wedding - 160.jpg",
+  "images/thumbnail/wedding/Shi & Jie's wedding-42.jpg",
+  "images/thumbnail/wedding/Shi & Jie's wedding-34.jpg",
+  "images/thumbnail/wedding/Luo & Ye's wedding _ 90.jpg",
+  "images/thumbnail/wedding/Zhang & Ying's wedding _ 70.jpg",
+  "images/thumbnail/wedding/Shi & Jie's wedding-08.jpg",
+  "images/thumbnail/wedding/Han Feng & Chen Hui's wedding - 084.jpg",  
+  "images/thumbnail/wedding/Luo & Ye's wedding _ 91.jpg",
+  "images/thumbnail/wedding/Shi & Jie's wedding-15.jpg",
+  "images/thumbnail/wedding/Han Feng & Chen Hui's wedding - 147.jpg",
+  "images/thumbnail/wedding/Zhang & Ying's wedding _ 64.jpg",
+  "images/thumbnail/wedding/Luo & Ye's wedding _ 73.jpg",
+  "images/thumbnail/wedding/Zhang & Ying's wedding _ 53.jpg",
+  "images/thumbnail/wedding/Luo & Ye's wedding _ 53.jpg",
+  "images/thumbnail/wedding/Huang & Chen's wedding _ 60.jpg",
+  "images/thumbnail/wedding/Han Feng & Chen Hui's wedding - 056.jpg",
+  "images/thumbnail/wedding/Zhang & Ying's wedding _ 58.jpg",
+  "images/thumbnail/wedding/Wu & Deng's wedding-14.jpg",
+  "images/thumbnail/wedding/Jan & Leo's Wedding_62.jpg",
+  "images/thumbnail/wedding/Wu & Deng's wedding-44.jpg",
+  "images/thumbnail/wedding/Zhang & Ying's wedding _ 38.jpg",
+  "images/thumbnail/wedding/Jan & Leo's Wedding_55.jpg",
+  "images/thumbnail/wedding/Luo & Ye's wedding _ 13.jpg",   
+  "images/thumbnail/wedding/Wu & Deng's wedding-39.jpg",   
+  "images/thumbnail/wedding/Han Feng & Chen Hui's wedding - 039.jpg",
+  "images/thumbnail/wedding/Jan & Leo's Wedding_34.jpg",
+  "images/thumbnail/wedding/Jan & Leo's Wedding_53.jpg",
+  "images/thumbnail/wedding/Huang & Chen's wedding _ 69.jpg",
+  "images/thumbnail/wedding/Luo & Ye's wedding _ 05.jpg",
+  "images/thumbnail/wedding/Wu & Deng's wedding-06.jpg",
+  "images/thumbnail/wedding/Zhang & Ying's wedding _ 29.jpg",
+  "images/thumbnail/wedding/Jan & Leo's Wedding_58.jpg",
+  "images/thumbnail/wedding/Jan & Leo's Wedding_76.jpg",
+  "images/thumbnail/wedding/Luo & Ye's wedding _ 15.jpg",  
+  "images/thumbnail/wedding/Chen & Li's wedding _ 061.jpg",
+];
+
+//add new wedding album on top
+const babyNKidAlbum = [
+  "images/thumbnail/baby/Nico's 1 month _ 06.jpg",
+  "images/thumbnail/baby/1Y-01.jpg",
+  "images/thumbnail/baby/Nico's 1 month _ 07.jpg",
+  "images/thumbnail/baby/1Y-02.jpg",
+  "images/thumbnail/baby/4M-04.jpg",
+  "images/thumbnail/baby/Nico's 1 month _ 05.jpg",  
+  "images/thumbnail/baby/Zhu.jpg",
+  "images/thumbnail/baby/Nico's 1 month _ 02.jpg",
+  "images/thumbnail/baby/Momo 8M.jpg",
+  "images/thumbnail/baby/Momo 11M-01.jpg",
+  "images/thumbnail/baby/Nico's 1 month _ 03.jpg",
+  "images/thumbnail/baby/Momo 11M-02.jpg",
+  "images/thumbnail/baby/Momo 11M-03.jpg",
+  "images/thumbnail/baby/5M-02.jpg",
+  "images/thumbnail/baby/Nico's 1 month _ 04.jpg",
+  "images/thumbnail/baby/Nico's 1 month _ 01.jpg",
+];
+
+//add new famliy album on top
+const familyAlbum = [ 
+  "images/thumbnail/family/Xu & An's family_01.jpg",
+  "images/thumbnail/family/Xu & An's family_02.jpg",
+  "images/thumbnail/family/Xu & An's family_03.jpg",
+  "images/thumbnail/family/Xu & An's family_04.jpg",
+  "images/thumbnail/family/Xu & An's family_05.jpg",
+  "images/thumbnail/family/Xu & An's family_06.jpg", 
+  "images/thumbnail/family/DSC_1214.jpg",
+  "images/thumbnail/family/DSC_8263.jpg",
+  "images/thumbnail/family/DSC_8269.jpg",
+  "images/thumbnail/family/DSC_9060.jpg",
+  "images/thumbnail/family/DSC_2246.jpg",
+   
+];
+
+//add new pregnancy album on top
+const pregnancyAlbum = [
+  "images/thumbnail/pregnancy/Hannah_02.jpg",
+  "images/thumbnail/pregnancy/Hannah_04.jpg",
+  "images/thumbnail/pregnancy/Hannah_10.jpg",
+  "images/thumbnail/pregnancy/Hannah_09.jpg",
+  "images/thumbnail/pregnancy/Hannah_11.jpg",
+  "images/thumbnail/pregnancy/Hannah_12.jpg",
+  "images/thumbnail/pregnancy/Hannah_01.jpg",
+  "images/thumbnail/pregnancy/Hannah_03.jpg",
+  "images/thumbnail/pregnancy/Hannah_15.jpg",
+  "images/thumbnail/pregnancy/Hannah_16.jpg",
+  "images/thumbnail/pregnancy/Hannah_13.jpg",
+  "images/thumbnail/pregnancy/Hannah_14.jpg",
+  "images/thumbnail/pregnancy/Hannah_17.jpg",
+  "images/thumbnail/pregnancy/Hannah_05.jpg",
+  "images/thumbnail/pregnancy/Hannah_08.jpg",
+  "images/thumbnail/pregnancy/Hannah_07.jpg",
+  "images/thumbnail/pregnancy/Hannah_06.jpg",
+  "images/thumbnail/pregnancy/Hannah_18.jpg",
+  "images/thumbnail/pregnancy/Hannah_19.jpg",
+  "images/thumbnail/pregnancy/Hannah_20.jpg",
+  "images/thumbnail/pregnancy/Y & T_01.jpg",
+  "images/thumbnail/pregnancy/Y & T_02.jpg",
+  "images/thumbnail/pregnancy/Y & T_03.jpg",
+  "images/thumbnail/pregnancy/Y & T_04.jpg",
+  "images/thumbnail/pregnancy/Y & T_05.jpg",  
+  "images/thumbnail/pregnancy/Y & T_06.jpg",
+  
+];
+
+//all album = wedding + baby + family
+const allAlbum = familyAlbum.concat(babyNKidAlbum,weddingAlbum,pregnancyAlbum);
+
+// Animate Smooth Scroll (jQuery)
+$(document).ready(()=> {
+  $("#view-work").on("click", function() {
+    const images = $("#images").position().top;
+
+    $("html, body").animate(
+      {
+        scrollTop: images
+      },
+      600
+    );
+  });
+
+  $("#nav-logo").on("click", function() {
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      600
+    );
+  });
+
+  $("#nav-album").on("click", function() {
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      600
+    );
+  });
+
+  $("#nav-contact").on("click", function() {
+    const contact = $("#main-footer").position().top;
+
+    $("html, body").animate(
+      {
+        scrollTop: contact
+      },
+      600
+    );
+  }); 
+
+  //$("img.lazyload").lazyload(); 
+
+  //click thumbdiv to see large size images
+  $("#masonry").on("click",".thumbDiv",function() {
+    let imgSrc = $(this).children().attr("src");//input img on click as _this    
+    let imgIndex = $(this).children().attr("name"); 
+    imgShow("#largeSizePhotoContainter", "#largeSizePhotoContainterInner", "#largeSizePhoto", imgSrc, imgIndex);
+  });  
+
+  //press arrow to see prev or next large size images
+  $(".arrow").on("click",function(){
+    let direction = $(this).attr("id");
+    let imgAdjacentSrcLargeSize = imgAdjacent(direction);   
+    let imgAdjacentIndex = $(this).attr("name");
+    imgShow("#largeSizePhotoContainter", "#largeSizePhotoContainterInner", "#largeSizePhoto", imgAdjacentSrcLargeSize, imgAdjacentIndex);
+  });
+
+  //use left & right arrow down to see different large size images, press Esc to hide large size images
+  $("html, body").keydown(function(){
+    let imgAdjacentSrcLargeSize;
+    let imgAdjacentIndex = $("#largeSizePhoto").attr("name");
+    if(event.keyCode == 37){ //left arrow down
+      imgAdjacentSrcLargeSize = imgAdjacent("prev");
+    }else if(event.keyCode == 39){ //right arrow down
+      imgAdjacentSrcLargeSize = imgAdjacent("next");
+    }else if(event.keyCode == 27){
+      $("#largeSizePhotoContainter").hide();
+    }    
+    imgShow("#largeSizePhotoContainter", "#largeSizePhotoContainterInner", "#largeSizePhoto", imgAdjacentSrcLargeSize, imgAdjacentIndex);
+  });
+
+
+  $(function () {
+    if(document.createEvent("TouchEvent")){
+      $("#portfolioTag").css("display","block");
+      $("#masonry").css("padding-top","0");
+    }else {
+      $("#portfolioTag").css("display","none");
+      $("#masonry").css("padding-top","4rem");
+    };
+
+    $(window).scroll(function(){
+        if ($(window).scrollTop()>100){
+            $("#back-to-top").fadeIn(500);
+        }
+        else
+        {
+            $("#back-to-top").fadeOut(500);
+        }
+    });
+
+    //back to top when click link
+    $("#back-to-top").click(function(){
+        //$('body,html').animate({scrollTop:0},1000);
+    if ($('html').scrollTop()) {
+        $('html').animate({ scrollTop: 0 }, 1000);
+        return false;
+    }
+    $('body').animate({ scrollTop: 0 }, 1000);
+         return false;            
+   });        
+});    
+
+});
+
+//set homepage pictures
+var imagesDiv = document.getElementById("images");
+if (imagesDiv != null) {
+  var images = [
+    "images/homepage/Han Feng & Chen Hui's wedding - 144.jpg",
+    "images/homepage/New Plymouth.jpg",
+    "images/homepage/Hannah & Shane's wedding _ 10.jpg",
+    "images/homepage/Huang & Chen's wedding _ 20.jpg",
+    "images/homepage/Jan & Leo's Wedding_33.jpg",
+    "images/homepage/Shi & Jie's wedding-29.jpg",
+    "images/homepage/Zhang & Ying's wedding _ 55.jpg",    
+    "images/homepage/Han Feng & Chen Hui's wedding - 169.jpg",
+    "images/homepage/Luo & Ye's wedding _ 38.jpg",
+    "images/homepage/Zhang & Ying's wedding _ 75.jpg",
+    "images/homepage/Han Feng & Chen Hui's wedding - 069.jpg"    
+  ];
+  var titles = [
+    "Lombok Island",
+  ];
+
+  images.forEach(function(picture) {
+    var imageDiv = document.createElement("div");
+    imageDiv.innerHTML = '<img src="' + picture + '" alt="">';
+    imagesDiv.appendChild(imageDiv);
+
+    var textDiv = document.createElement("div");
+    //textDiv.innerHTML =
+      //"<h3>" +
+      //titles[images.indexOf(picture)] +
+      //"</h3> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita, laboriosam.</p>";
+    textDiv.setAttribute("class", "caption");
+    imagesDiv.appendChild(textDiv);
+  });
+}
+
+let albumType = getAlbumType();
+showAlbum(albumType);
+lazyload();
+
+
+//get album type depend on web tag
+function getAlbumType(){
+  let type = window.location.hash;
+  return type;
+};
+
+//get photo name between last "/" and the 1st "."
+function getImgName(imgAddress){
+  let photoAddressIndexStart = imgAddress.lastIndexOf("/") + 1;
+  let photoAddressIndexEnd = imgAddress.lastIndexOf(".");
+  let photoName = imgAddress.substring(photoAddressIndexStart,photoAddressIndexEnd);
+  return photoName;
+};
+
+//get album by album type
+function switchAlbum(albumType){
+  switch(albumType){
+    case "#wedding":
+        album = weddingAlbum;
+        break;
+    case "#baby":
+        album = babyNKidAlbum;
+        break;
+    case "#family":
+        album = familyAlbum;
+        break;
+    case "#pregnancy":
+        album = pregnancyAlbum;
+        break;
+    case "#all":
+        album = allAlbum;
+        break;
+  }
+  return album;
+}
+
+//use this function to show photo at certain location, use photoType and photo address array as 2 input
+function showAlbum(albumType) {
+      let masonryDiv = document.getElementById("masonry");
+      let album=[];
+      if(masonryDiv != null) {    
+        masonryDiv.innerHTML="";
+        album = switchAlbum(albumType);        
+        album.forEach(function(address,index) {
+          let photoDiv = document.createElement("div");
+          photoDiv.setAttribute("class","thumbDiv");
+
+          //get photo name between last "/" and the 1st "."
+          let imgName = getImgName(address);
+
+          //use lazyload to load imgs enters the browsers viewport
+          let img = new Image();
+          img.src = address;
+          img.onload=function(){
+            let width = img.width;
+            let height = img.height;            
+            photoDiv.innerHTML =  '<img src = "' + address + '" data-original = "' + address + '"alt = " '+ imgName + ' " class = "lazyload thumbnail" width = "'+width+'" height= "'+height+'" name ="'+index+'">';           
+          }          
+          masonryDiv.appendChild(photoDiv);
+        });
+      } 
+  };
+
+function imgShow(outerdiv, innerdiv, bigimg, imgSrc, imgIndex){  
+  let srcThumb = imgSrc;  
+
+  //get src of large size photo
+  let src= srcThumb.replace("thumbnail","largesize");   
+  $(bigimg).attr("src", src); //set large size address
+
+  let img = new Image();
+  img.src = src;
+  img.onload = function(){ 
+    let imgWidth, imgHeight, left, top;  //width and height after zoom in   
+    let windowWidth = $(window).width();
+    let windowHeight = $(window).height();
+    let originalHeight = img.height;
+    let originalWidth = img.width; 
+    const scale = 0.9; //zoom in rate   
+    
+    //zoom in photo depend on window height and weight
+    if(originalHeight >= windowHeight){
+      imgHeight = windowHeight * scale;
+      imgWidth = originalWidth * imgHeight / originalHeight;
+      if (imgWidth > windowWidth * scale){
+        imgWidth = windowWidth * scale;
+      }
+    }else if(originalWidth >= windowWidth * scale) {
+      imgWidth = windowWidth * scale;
+      imgHeight = originalHeight * imgWidth / originalWidth;
+    }else {
+      imgHeight = originalHeight;
+      imgWidth = originalWidth;
+    }     
+    $(bigimg).css({"width":imgWidth});
+    $(bigimg).attr("name",imgIndex); //set img index 
+
+    //center large size photo in window
+    left = (windowWidth - imgWidth)/2;
+    top = (windowHeight - imgHeight)/2;    
+    $(innerdiv).css({"top":top,"left":left});     
+    $(outerdiv).fadeIn("fast");
+
+    $(outerdiv).click(function(){
+      $(this).fadeOut("fast");
+    }); 
+  };  
+}  
+
+function imgAdjacent(direction){
+  let currentAlbumType = getAlbumType();  
+  let largeSizePhoto = document.getElementById("largeSizePhoto");
+  let currentThumbIndex = largeSizePhoto.name;
+  
+  let newThumbIndex, newThumbScr;
+  let currentAlbum = switchAlbum(currentAlbumType);
+  //get index of next or prev img depend on the button pressed
+  if(direction == "prev"){
+    if(currentThumbIndex == "0"){
+      newThumbIndex = currentAlbum.length - 1;
+    }else {
+      newThumbIndex = currentThumbIndex/1 - 1;
+    }
+  }else if(direction == "next"){
+    if(currentThumbIndex == currentAlbum.length - 1 ){
+      newThumbIndex = 0;
+    }else {
+      newThumbIndex = currentThumbIndex/1 + 1;
+    }
+  }  
+  newThumbScr = currentAlbum[newThumbIndex];
+  largeSizePhoto.name = newThumbIndex; //set largesize img's name as new thumbnail's index
+  let newLargesizeSrc = newThumbScr.replace("thumbnail","largesize");
+  return newLargesizeSrc;  
+}
